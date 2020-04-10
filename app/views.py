ec3bef2cdf133ce95adf0d4c088a7f6756463dff
Dommel Iona <iona.dommel-prioux@epitech.eu>
2020-04-10 22:29:28 +0200

    Added stuff

--- app/views.py
+++ app/views.py
@@ -10,18 +10,27 @@ from app import app
 from  flask  import  jsonify
 import  pymysql  as sql
 
+user='root'
+
+
 @app.route('/user')
 def route_all_users ():
-    result = "Hey, it's ... ?"
     try:
+        insert_stuff = (
+            "INSERT INTO user (username, password)\
+            VALUES ('bob','bob')"
+        )
         connect = sql.connect(host='localhost',
-                            unix_socket='path_to_our_mysql_socket',
-                            user='_user',
-                            passwd='_password',
-                            db='database_test1')
-        cursor = connect.cursor ()
-        cursor.execute("SELECT * from  user")
+                        unix_socket='/var/lib/mysql/mysql.sock',
+                        user=user,
+                        passwd='Fnzatpez1.',
+                        db='new_schema')##connectionbd
+        cursor = connect.cursor ()##ini de la requete
+        cursor.execute(insert_stuff)##execution de la requete
+        cursor.execute("SELECT * FROM user")##execution de la requete
+
         result = cursor.fetchall ()
+        connect.commit()
         cursor.close()
         connect.close()
     except Exception as e :
