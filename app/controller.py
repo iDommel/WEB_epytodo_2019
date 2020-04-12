b5f153a35c18d484bcdb09ba9288b1cf11506ed7
Zac <izaac.carcenac-sautron@epitech.eu>
2020-04-12 18:40:08 +0200

    Added: Patrickkkkkkkk ... à la trick

--- app/controller.py
+++ app/controller.py
@@ -11,14 +11,14 @@ from flask import render_template, request
 import pymysql as sql
 
 
-pwd = 'Fnzatpez1.'
+pwd = 'sdfmovieconquest1'
 user = 'root'
 
 class Controller(object):
 
     def __init__(self, title, myContent):
         self.title = title
-        self.myContent = myContent
+        self.user_id = 1
 
     def index_action(self):
         return render_template("index.html",
@@ -38,9 +38,9 @@ class Controller(object):
                             unix_socket='/var/lib/mysql/mysql.sock',
                             user=user,
                             passwd=pwd,
-                            db='epytodo')##connect to database
+                            db='epytodo')
             cursor = connect.cursor()
-            cursor.execute("SELECT * FROM task")##execution de la requete
+            cursor.execute("SELECT * FROM task")
             result = cursor.fetchall()
             connect.commit()
             cursor.close()
@@ -57,21 +57,34 @@ class Controller(object):
             task = request.form['task']
             begin = request.form['begin']
             end = request.form['end']
-            status = request.form['status']
         try:
             insert_stuff = (
-                "INSERT INTO task (title, begin, end, status) VALUES ('" + task + "','" + begin + "','" + end + "','" + status + "')"
+                "INSERT INTO task (title, begin, end, status) VALUES ('" + task + "','" + begin + "','" + end + "','0')"
             )
             connect = sql.connect(host='localhost',
                             unix_socket='/var/lib/mysql/mysql.sock',
                             user=user,
                             passwd=pwd,
-                            db='epytodo')##connect to database
+                            db='epytodo')
             cursor = connect.cursor()
-            cursor.execute(insert_stuff)##execution de la requete
+            cursor.execute(insert_stuff)
             connect.commit()
             cursor.close()
             connect.close()
+        except Exception as e :
+            print("Caught  an  exception : ", e)
+        try:
+                connect = sql.connect(host='localhost',
+                            unix_socket='/var/lib/mysql/mysql.sock',
+                            user=user,
+                            passwd=pwd,
+                            db='epytodo')
+                cursor = connect.cursor()
+                task_id = cursor.execute("SELECT task_id FROM task ORDER BY task_id DESC")
+                cursor.execute("INSERT INTO user_has_task (fk_user_id, fk_task_id) VALUES ('" + str(self.user_id) + "','" + task_id + "')")
+                connect.commit()
+                cursor.close()
+                connect.close()
         except Exception as e :
             print("Caught  an  exception : ", e)
         return self.display_task_action()
