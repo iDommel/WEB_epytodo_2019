07216eb5e32bbe7acd72e6a33c724e09d6a3eed6
Zac <izaac.carcenac-sautron@epitech.eu>
2020-04-12 21:01:51 +0200

    Added: models functions

--- app/controller.py
+++ app/controller.py
@@ -6,18 +6,14 @@
 ##
 
 from app import app
-from app.models import User, Task
+from app.models import models
 from flask import render_template, request
 import pymysql as sql
-
-
-pwd = 'sdfmovieconquest1'
-user = 'root'
 
 class Controller(object):
 
     def __init__(self, user_id):
-        self.user_id = user_id
+        self.models = models(user_id)
 
     def index_action(self):
         return render_template("index.html")
@@ -28,27 +24,12 @@ class Controller(object):
             password = request.form['password']
             if (len(username) == 0 or len(password) == 0):
                 return render_template("alerts/invalid_logs.html")
-            try:
-                insert_stuff = (
-                    "INSERT INTO user (username, password) VALUES ('" + username + "','" + password + "')"
-                )
-                connect = sql.connect(host='localhost',
-                                unix_socket='/var/lib/mysql/mysql.sock',
-                                user=user,
-                                passwd=pwd,
-                                db='epytodo')
-                cursor = connect.cursor()
-                cursor.execute(insert_stuff)
-                connect.commit()
-                cursor.close()
-                connect.close()
-            except Exception as e :
-                print("Caught  an  exception : ", e)
+            self.models.register_model(username, password)
         return render_template("register.html")
 
     def signout_action(self):
-        if self.user_id != -1:
-            self.user_id = -1
+        if self.models.user_id != -1:
+            self.models.user_id = -1
             return render_template("alerts/logged_out.html", username = self.username)
         return render_template("/index.html")
 
@@ -56,47 +37,14 @@ class Controller(object):
         if request.method == 'POST':
             username = request.form['username']
             password = request.form['password']
+            self.username = username
             if (len(username) == 0 or len(password) == 0):
                 return render_template("alerts/invalid_logs.html")
-            try:
-                connect = sql.connect(host='localhost',
-                                unix_socket='/var/lib/mysql/mysql.sock',
-                                user=user,
-                                passwd=pwd,
-                                db='epytodo')
-                cursor = connect.cursor()
-                cursor.execute("SELECT * FROM user")
-                result = cursor.fetchall()
-                connect.commit()
-                cursor.close()
-                connect.close()
-                for i in result:
-                    if i[1] == username:
-                        if i[2] == password:
-                            self.user_id = i[0]
-                            self.username = username
-                            return render_template("alerts/logged_in.html", username = self.username)
-                return render_template("alerts/invalid_logs.html")
-            except Exception as ex :
-                print("Caught  an  exception : ", ex)
+            return self.models.sign_in_model(username, password)
         return render_template("signin.html")
 
     def display_task_action(self):
-        try:
-            connect = sql.connect(host='localhost',
-                            unix_socket='/var/lib/mysql/mysql.sock',
-                            user=user,
-                            passwd=pwd,
-                            db='epytodo')
-            cursor = connect.cursor()
-            cursor.execute("SELECT * FROM task")
-            result = cursor.fetchall()
-            connect.commit()
-            cursor.close()
-            connect.close()
-        except Exception as ex :
-            print("Caught  an  exception : ", ex)
-            result = 0
+        result = self.models.display_task_model()
         return render_template("tasks_view.html",
                                 tables = result)
 
@@ -105,26 +53,6 @@ class Controller(object):
             task = request.form['task']
             begin = request.form['begin']
             end = request.form['end']
-        try:
             if (task and begin and end):
-                connect = sql.connect(host='localhost',
-                                unix_socket='/var/lib/mysql/mysql.sock',
-                                user=user,
-                                passwd=pwd,
-                                db='epytodo')
-                cursor = connect.cursor()
-                cursor.execute("INSERT INTO task (title, begin, end, status) VALUES ('" + task + "','" + begin + "','" + end + "','0')")
-                connect.commit()
-                try:
-                    task_id = cursor.execute("SELECT task_id FROM task ORDER BY task_id DESC")
-                    cursor.execute("INSERT INTO user_has_task (fk_user_id, fk_task_id) VALUES ('" + str(self.user_id) + "','" + str(task_id) + "')")
-                except Exception as e :
-                    print("Caught  an  exception : ", e)
-                    print("has_task_fail")
-                connect.commit()
-                cursor.close()
-                connect.close()
-        except Exception as e :
-            print("Caught  an  exception : ", e)
-            print("add_task_fail")
+                self.models.task_create_model(task, begin, end)
         return self.display_task_action()
